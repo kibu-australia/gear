f4509bc5cf4bb7ec38245eb04edaa09dd7c9c7df
Thomas Crowley <crowley@kibu.com.au>
2014-06-25 16:06:12 +1000

    Add namespace for calculations

--- project.clj
+++ project.clj
@@ -9,16 +9,18 @@
                  [reagent "0.4.2"]
                  [com.keminglabs/cljx "0.4.0"]]
   :profiles {:dev 
-             {:dependencies [[figwheel "0.1.3-SNAPSHOT"]]
+             {:dependencies [[figwheel "0.1.3-SNAPSHOT"]
+                             [com.cemerick/piggieback "0.1.3"]]
+              :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
               :plugins [[com.cemerick/clojurescript.test "0.3.1"]
                         [lein-cljsbuild "1.0.3"]
                         [lein-figwheel "0.1.3-SNAPSHOT"]]
               :test-commands {"unit-tests" ["phantomjs" :runner
                                             "target/cljs/testable.js"]}
-              :cljsbuild {:builds [{:id "dev"
-                                    :source-paths ["src/cljs"]
-                                    :compiler {:preamble ["reagent/react.js"]
-                                               :output-to "resources/public/js/gear.js"
-                                               :output-dir "resources/public/js/out"
-                                               :source-map true
-                                               :optimizations :none}}]}}})
+              :cljsbuild
+              {:builds [{:id "dev"
+                         :source-paths ["src/cljs"]
+                         :compiler {:output-to "resources/public/js/gear.js"
+                                    :output-dir "resources/public/js/out"
+                                    :source-map true
+                                    :optimizations :none}}]}}})
