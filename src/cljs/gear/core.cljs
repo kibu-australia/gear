7f4bb2cc34c9b72ab89b1f1bf9ecb51ba3e56bd3
Thomas Crowley <crowley@kibu.com.au>
2014-06-25 17:44:16 +1000

    Add basic slider for gear inch calculation

--- src/cljs/gear/core.cljs
+++ src/cljs/gear/core.cljs
@@ -2,8 +2,32 @@
   (:require [gear.calculator :as calc]
             [reagent.core :as reagent :refer [atom]]))
 
-(defn some-component []
-  [:div [:h3 "Turn up"]])
+(def chainring (atom 42))
+(def sprocket (atom 16))
+(def wheel (atom 27))
 
-(reagent/render-component [some-component]
-                            (.-body js/document))
+
+(defn slider [value rng]
+  [:input {:type "range"
+           :value @value
+           :min (apply min rng)
+           :max (apply max rng)
+           :style {:width "100%"}
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn my-component []
+  [:div
+   [:h3 "Gear Ratio Calculator"]
+   [:div
+    "Chainring: " @chainring
+    [slider chainring calc/chainring]]
+   [:div "Sprocket: " @sprocket
+    [slider sprocket calc/sprockets]]
+   [:div
+    [:strong
+     "= "
+     (calc/gear-inch @wheel @chainring @sprocket)
+     " gear inches"]]])
+
+(reagent/render-component [my-component]
+                          (.-body js/document))
