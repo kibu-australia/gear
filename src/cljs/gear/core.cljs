b3a1ae893297f1e2f8f9619b15675ef24569d795
Thomas Crowley <crowley@kibu.com.au>
2014-06-27 22:58:01 +1000

    Add "Suggest" page
    Suggestions are based on the suggest-ratio function. The user
    inputs a desired gear inch and is returned a list of chainring/srocket
    combinations nearest.
    
    Added in navigation to page. UI still needs to be polished.

--- src/cljs/gear/core.cljs
+++ src/cljs/gear/core.cljs
@@ -8,6 +8,7 @@
 
 (def chainring (atom 42))
 (def sprocket (atom 16))
+(def inch (atom (apply min inches)))
 (def wheel (atom 27))
 
 (defn slider [value range]
@@ -18,9 +19,16 @@
            :style {:width "100%"}
            :on-change #(reset! value (-> % .-target .-value))}])
 
+(defn input-number [value]
+  [:input {:type "number"
+           :min (apply min inches)
+           :max (apply max inches)
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value js/Number))}])
+
 (defn gear-ratio []
   (-> (calc/gear-inch @wheel @chainring @sprocket)
-              (.toFixed 2)))
+      (.toFixed 2)))
 
 (defn my-calculator []
   [:div
@@ -30,5 +38,28 @@
     [slider sprocket sprockets]]
    [:div [:h1.result (gear-ratio) " gear inches"]]])
 
-(reagent/render-component [my-calculator]
-                          (.getElementById js/document "main"))
+(defn my-navigation []
+  [:ul.nav
+   [:li {:on-click render-calculator} "Calculate"]
+   [:li {:on-click render-suggestions} "Suggest"]])
+
+(defn my-suggestion []
+  [:div
+   [:div [input-number inch]]
+   [:div (when @inch
+           (str (calc/suggest-ratio @wheel @inch chainrings sprockets)))]])
+
+(defn render [id body]
+  (reagent/render-component body (.getElementById js/document id)))
+
+(defn render-calculator []
+  (render "main" [my-calculator]))
+
+(defn render-suggestions []
+  (render "main" [my-suggestion]))
+
+(defn render-nav []
+  (render "nav" [my-navigation]))
+
+(render-calculator)
+(render-nav)
